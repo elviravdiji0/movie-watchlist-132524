1e1529b2419eb79b9f8fde76198eeb8d5be8c98d
elviravdiji0 <elviravdiji0@gmail.com>
2026-06-01 21:42:28 +0200

    Updated Navbar with auth state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import { Link } from 'react-router-dom'
-
-// TODO: import and use AuthContext here to show conditional links
+import { AuthContext } from '../context/AuthContext'
+import { useContext } from 'react'
 
 function Navbar() {
+  const { user, logout } = useContext(AuthContext);
+
   return (
     <nav className="bg-gray-900 text-white px-6 py-4 flex items-center justify-between">
       <Link to="/" className="font-bold text-lg">Movie Watchlist</Link>
       <div className="flex gap-4 text-sm">
-        {/* TODO: if no user is logged in, show a Login link */}
-        {/* TODO: if a user is logged in, show a Welcome message and a Logout button */}
-        <Link to="/login" className="hover:underline">Login</Link>
+        {user && <div className='flex gap-2'><span>Hello {user.username}</span><button onClick={logout}>Logout</button></div>}
+        {(!user) && <Link to="/login" className="hover:underline">Login</Link>}
       </div>
     </nav>
   )
